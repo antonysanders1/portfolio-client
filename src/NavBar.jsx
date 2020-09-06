290ded7b18aa15e85b5ccef98d4050a6f2559809
antony_sanders <antony.devon92@gmail.com>
2020-09-05 22:13:20 -0400

    added styling to the Nav Bar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,25 +1,42 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
 import {Grid, Typography, Divider, Button} from '@material-ui/core'
+import { withStyles } from '@material-ui/core/styles';
 
 import Home from './components/Home'
 import About from './components/About'
 import Login from './components/Login'
 
+const styles = () => ({
+    navLogo: {
+      color: 'black',
+      textDecoration: 'none',
+      fontSize: '40px',
+      fontWeight: 'bold'
+    },
+    naviBar: {
+        color: 'black',
+        textDecoration: 'none',
+        fontSize: '40px',
+        fontWeight: 'bold'
+      },
+  });
+
 
 class NavBar extends React.Component {
   render(){
+    const {classes} = this.props;
     return (
       <div>
           <Router>
-                    <Grid container direction="column">
+                    <Grid container direction="column" className={classes.naviBar}>
                         <Grid item xs={12} container spacing={2}>
                         
                             <Grid item lg={1}> </Grid>
 
 
                             <Grid item xs={4} sm={3} md={2}>
-                            <Typography className="link" variant='h5'>Portfoli-Yo!</Typography>
+                            <Typography className={classes.navLogo} variant='h5'>Portfoli-Yo!</Typography>
                             </Grid>
 
                             <Grid item sm={2} md={3} lg={6}> </Grid>
@@ -49,4 +66,4 @@ class NavBar extends React.Component {
   }
 }
 
-export default NavBar;
+export default withStyles(styles)(NavBar);
