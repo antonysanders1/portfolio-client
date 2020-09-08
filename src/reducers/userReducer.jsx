40f3a0779fa70b0ec9c8268e6989d00d409b48b8
antony_sanders <antony.devon92@gmail.com>
2020-09-08 03:42:52 -0400

    1. created a userReducer to house the user state and actions, 2. created a rootReducer to manage all reducers

---
+++ src/reducers/userReducer.jsx
@@ -0,0 +1,17 @@
+export default (state = {}, action) => {
+    switch (action.type) {
+        case 'CREATE_USER':
+            return (action.payload)
+
+        case 'LOGIN_USER':
+            return (action.payload)
+
+        case 'LOGOUT_USER':
+            return {}
+
+        case 'CURRENT_USER':
+            return (action.user)
+        default:
+            return state;
+    };
+};
