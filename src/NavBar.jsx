93432ae7d6282071b265d2760c2b177159d7449b
antony_sanders <antony.devon92@gmail.com>
2020-09-08 03:04:50 -0400

    1.passed in user props, 2.added conditions to the navbar button display, 3. added a home path, 4.added recompose

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
-import {Grid, Typography, Divider, Button} from '@material-ui/core'
+import { BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+import {Grid, Typography, Divider, Button} from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
+import {compose} from 'recompose';
+import {connect} from 'react-redux';
 
-import Home from './components/Home'
-import About from './components/About'
-import Login from './components/Login'
+import Home from './components/Home';
+import About from './components/About';
+import Login from './components/Login';
+import UserContainer from './containers/UserContainer';
 
 const styles = () => ({
     navLogo: {
@@ -24,41 +27,55 @@ const styles = () => ({
 
 
 class NavBar extends React.Component {
+    componentDidMount() {
+        console.log(this.props)
+    }
+
   render(){
     const {classes} = this.props;
     return (
       <div>
           <Router>
                     <Grid container direction="column" className={classes.naviBar}>
-                        <Grid item xs={12} container spacing={2}>
+                        <Grid item xs={12} container >
                         
-                            <Grid item lg={1}> </Grid>
+                            <Grid item md={1}> </Grid>
 
 
-                            <Grid item xs={4} sm={3} md={2}>
+                            <Grid item xs={6} sm={5} md={3} lg={3}>
                             <Typography className={classes.navLogo} variant='h5'>Portfoli-Yo!</Typography>
                             </Grid>
 
-                            <Grid item sm={2} md={3} lg={6}> </Grid>
+                            <Grid item sm={1} md={1} lg={5}> </Grid>
 
-                            <Grid item xs={2} sm={2} md={2} lg={1}>  
-                                <Button fullWidth color='primary' component={Link} to="/">Home</Button>
-                            </Grid>
-
-                            <Grid item xs={3} sm={2} md={2} lg={1}>
-                                <Button fullWidth color='primary' component={Link} to="/about">About</Button>
+                            <Grid item xs={2} sm={2} md={2} lg={1}> 
+                                {
+                                  this.props.user.length > 0 ? null :
+                                  <Button fullWidth color='primary' component={Link} to="/">Home</Button>
+                                } 
                             </Grid>
 
                             <Grid item xs={2} sm={2} md={2} lg={1}>
+                                {
+                                  this.props.user.length > 0 ? null :
+                                  <Button fullWidth color='primary' component={Link} to="/about">About</Button>
+                                }
+                            </Grid>
+
+                            <Grid item xs={2} sm={2} md={2} lg={1}>
+                                {
+                                this.props.user.length > 0 ? <Button fullWidth color='primary' component={Link} to="/">Logout</Button> : 
                                 <Button fullWidth color='primary' component={Link} to="/login">Login</Button>
+                                } 
                             </Grid>       
                         </Grid>
                         <Divider/>
                     </Grid>
                     <Switch>
-                        <Route path="/login" component={Login} />
-                        <Route path="/about" component={About} />
-                        <Route path="/" component={Home} />
+                        <Route path="/home" exact component={UserContainer} />
+                        <Route path="/login" exact component={Login} />
+                        <Route path="/about" exact component={About} />
+                        <Route path="/" exact component={Home} />
                     </Switch>
             </Router>
       </div>
@@ -66,4 +83,12 @@ class NavBar extends React.Component {
   }
 }
 
-export default withStyles(styles)(NavBar);
+const mapState = ({user}) => {
+    return {
+        user
+    }
+}
+
+export default compose(
+  withStyles(styles, {name: 'App',}),
+  connect(mapState))(NavBar);
