475519b8459a40172acb5659ddfddae108cb0038
antony_sanders <antony.devon92@gmail.com>
2020-09-09 06:56:54 -0400

    added Feed component

---
+++ src/components/Feed.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Feed extends Component {
+render() {
+        return (
+            <div>
+                Hello World from the Feed!
+            </div>
+        )
+    }
+}
+
+export default Feed;
