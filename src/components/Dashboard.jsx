4d741c9fff896cba9dc00348b75292ef9fd81275
antony_sanders <antony.devon92@gmail.com>
2020-09-09 06:48:04 -0400

    added user profile pic from backend

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -34,7 +34,7 @@ const DashBoard = (props) => {
         <Grid container direction="column" className={classes.main}>
             <Grid container direction="row" className={classes.user}>
                 <Grid item xs={5}>
-                    <Avatar alt="Remy Sharp" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" className={classes.image}/>
+                    <Avatar alt="Profile Picture" src={`http://localhost:3000${props.user.avatar}`} className={classes.image}/>
                 </Grid>
                 <Grid container item xs={7} direction="column"  justify="space-evenly">
                     <Grid>
