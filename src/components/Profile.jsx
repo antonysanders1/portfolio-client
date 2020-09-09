731d351cf932074afeed2edce61100fbadf5d3d7
antony_sanders <antony.devon92@gmail.com>
2020-09-09 16:18:00 -0400

    Created profile component

---
+++ src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Profile extends Component {
+render() {
+        return (
+            <div>
+                Hello World from the Profile!
+            </div>
+        )
+    }
+}
+
+export default Profile;
