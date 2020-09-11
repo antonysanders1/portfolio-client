b8f1a90e6eb20d6821cb780c411cdba3e0559f1e
antony_sanders <antony.devon92@gmail.com>
2020-09-11 00:25:01 -0400

    addded styling to the container

--- src/containers/FeedContainer.jsx
+++ src/containers/FeedContainer.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {compose} from 'recompose';
 import { withStyles } from '@material-ui/core/styles';
-import {Grid} from '@material-ui/core';
-
-//import {getUser, logoutUser} from '../actions/actions';
+import {Grid, Typography} from '@material-ui/core';
 
 import DashBoard from '../components/Dashboard'
 import Feed from '../components/Feed'
@@ -17,14 +15,24 @@ const styles = (theme) => ({
     flexDirection: 'column',
     alignItems: 'center',
   },
-  section: {
-      background: "red",
-      height: '90vh',
-      paddingTop: theme.spacing(3),
-      paddingLeft: theme.spacing(3),
-      paddingRight: theme.spacing(3),
-      paddingBottom: theme.spacing(1),
-      border: 'solid, black, 1px'
+  section1: {
+    background: "#e8e8e8",
+    height: '90vh',
+    paddingTop: theme.spacing(3),
+    paddingLeft: theme.spacing(3),
+    paddingRight: theme.spacing(3),
+    paddingBottom: theme.spacing(1),
+    border: 'solid, black, 1px'
+  },
+  section2: {
+    height: '90vh',
+    overflow: 'hidden',
+    overflowY: 'scroll',
+  },
+  pgTitle: {
+    marginTop:'20px',
+    marginBottom: '20px',
+    fontWeight: 'bold'
   },
   avatar: {
     margin: theme.spacing(1),
@@ -40,12 +48,44 @@ const styles = (theme) => ({
   profile: {
     height: "20%",
     background: "blue"
-}
+  },
+  container: {
+    height: '100%'  
+  },
+  workContainer: {
+    height: '70vh',
+    padding: theme.spacing(1),
+    marginBottom: '30px',
+    // overflow: 'hidden',
+    flexGrow: 1,
+  },
+  imgContainer: {
+    height: '80%',
+    width: '100%',
+    display: 'block',
+    textAlign: 'center',
+    backgroundColor: '#e8e8e8'
+  },
+  img: {
+    height: '100%',
+    width: '100%',
+    objectFit: 'contain'
+  },
+  unclickedBtn: {
+    height: '24px',
+    width: '24px',
+    background: 'none',
+    color: 'grey'
+  },
+  header: {
+    display: 'block',
+    marginLeft: '10px'
+  }
 });
 
-class FeedContainer extends React.Component {
+class FeedContainer extends React.PureComponent {
     componentDidMount() {
-        console.log(this.props)
+      console.log(this.props)
     }
 
     render() {
@@ -53,7 +93,7 @@ class FeedContainer extends React.Component {
         return(
             <Grid container>
 
-                <Grid container item className={classes.section} direction="row" xs={12} sm={6} md={4} lg={3} xl={3}>
+                <Grid container item className={classes.section1} direction="row" xs={12} sm={6} md={4} lg={3} xl={3}>
                   <Grid container item direction="column">
                     <DashBoard 
                       classes ={classes}
@@ -61,8 +101,14 @@ class FeedContainer extends React.Component {
                     />
                   </Grid>
                 </Grid>
-                <Grid container item direction="row"xs={12} sm={6} md={8} lg={9} xl={9}>
-                  <Feed/>
+                <Grid container item className={classes.section2} direction="row"xs={12} sm={6} md={8} lg={9} xl={9}>
+                  <Grid container item xs={12} alignItems="center" justify="center" className={classes.pgTitle} >
+                    <Typography variant='h4'>FEED</Typography>
+                  </Grid>
+                  <Feed
+                    classes ={classes}
+                    works={this.props.works}
+                  />
                 </Grid>
             </Grid>
             
@@ -70,8 +116,8 @@ class FeedContainer extends React.Component {
     }
 }
 
-const mapState = ({user}) => {
-  return {user}
+const mapState = ({user, works}) => {
+  return {user, works}
 }
 
 export default compose(
