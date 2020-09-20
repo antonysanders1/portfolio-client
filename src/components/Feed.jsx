9d5cfce883c3bc0f369309f5e64b6cf0428db277
antony_sanders <antony.devon92@gmail.com>
2020-09-20 04:03:05 -0400

    Changed from class component to functional component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,25 +1,24 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Grid, CssBaseline, Avatar, Typography } from '@material-ui/core'
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 //import FavoriteIcon from '@material-ui/icons/Favorite';
 
-class Feed extends Component {
-render() {
+export default function Feed(props){
         return (
             <Grid container>
-                {this.props.works.map((work) => 
-                    <Grid container item xs={12} className={this.props.classes.workContainer} key={work.user + work.id}>
+                {props.works.map((work) => 
+                    <Grid container item xs={12} className={props.classes.workContainer} key={work.user + work.id}>
                         <CssBaseline/>
                     
                         <Grid item xs={1} md={2} lg={3}></Grid>
 
-                        <Grid container item xs={10} md={8} lg={6} direction='column' className={this.props.classes.container}>
-                            <Grid container className={this.props.classes.container}> 
+                        <Grid container item xs={10} md={8} lg={6} direction='column' className={props.classes.container}>
+                            <Grid container className={props.classes.container}> 
                                 <Grid item xs={2} lg={1}>
                                     <Avatar alt={`Profile Picture - ${work.user.name}`} src={`http://localhost:3000${work.user.avatar}`}></Avatar>
                                 </Grid>
 
-                                <Grid container item xs={9} lg={10} direction="column" className={this.props.classes.header}>
+                                <Grid container item xs={9} lg={10} direction="column" className={props.classes.header}>
                                     <Grid item xs={12}>
                                         <Typography variant="h6">{work.user.name}</Typography>
                                     </Grid>
@@ -28,13 +27,13 @@ render() {
                                     </Grid>
                                 </Grid>
 
-                                <Grid container item xs={12} className={this.props.classes.imgContainer}>
-                                    <img src={`http://localhost:3000${work.image}`} alt={work.title} className={this.props.classes.img}/>
+                                <Grid container item xs={12} className={props.classes.imgContainer}>
+                                    <img src={`http://localhost:3000${work.image}`} alt={work.title} className={props.classes.img}/>
                                 </Grid>
                                 <Grid container>
 
                                 <Grid item xs={1}>
-                                        <Avatar className={this.props.classes.unclickedBtn}>
+                                        <Avatar className={props.classes.unclickedBtn}>
                                             <FavoriteBorderIcon/>
                                         </Avatar>
                                     </Grid>
@@ -55,8 +54,66 @@ render() {
                     </Grid>
                 )}
             </Grid>
-        )
+        )   
     }
-}
 
-export default Feed;
+//--------------------------------------------------------------------------------- 
+//------------------------------------------------------ Class Component Equivelant
+
+// class Feed extends React.Component {
+// render() {
+//         return (
+//             <Grid container>
+//                 {this.props.works.map((work) => 
+//                     <Grid container item xs={12} className={this.props.classes.workContainer} key={work.user + work.id}>
+//                         <CssBaseline/>
+                    
+//                         <Grid item xs={1} md={2} lg={3}></Grid>
+
+//                         <Grid container item xs={10} md={8} lg={6} direction='column' className={this.props.classes.container}>
+//                             <Grid container className={this.props.classes.container}> 
+//                                 <Grid item xs={2} lg={1}>
+//                                     <Avatar alt={`Profile Picture - ${work.user.name}`} src={`http://localhost:3000${work.user.avatar}`}></Avatar>
+//                                 </Grid>
+
+//                                 <Grid container item xs={9} lg={10} direction="column" className={this.props.classes.header}>
+//                                     <Grid item xs={12}>
+//                                         <Typography variant="h6">{work.user.name}</Typography>
+//                                     </Grid>
+//                                     <Grid item xs={12}>
+//                                         <Typography variant="subtitle2">{work.title}</Typography>
+//                                     </Grid>
+//                                 </Grid>
+
+//                                 <Grid container item xs={12} className={this.props.classes.imgContainer}>
+//                                     <img src={`http://localhost:3000${work.image}`} alt={work.title} className={this.props.classes.img}/>
+//                                 </Grid>
+//                                 <Grid container>
+
+//                                 <Grid item xs={1}>
+//                                         <Avatar className={this.props.classes.unclickedBtn}>
+//                                             <FavoriteBorderIcon/>
+//                                         </Avatar>
+//                                     </Grid>
+//                                     <Grid item xs={11}>
+//                                         <Typography>0 Likes</Typography>
+//                                     </Grid>
+//                                 </Grid>
+
+//                                 <Grid container item xs={12}>
+//                                     <Typography variant="caption text">{work.description}</Typography>
+//                                 </Grid>
+
+//                             </Grid>
+
+//                         </Grid>
+
+//                         <Grid item xs={1} md={2} lg={3}></Grid>
+//                     </Grid>
+//                 )}
+//             </Grid>
+//         )
+//     }
+// }
+
+// export default Feed;
