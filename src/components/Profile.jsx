9a00ca2b65c24b4542d4800b2c4bd9ef0aea6637
antony_sanders <antony.devon92@gmail.com>
2020-09-20 04:04:07 -0400

    Changed from class component to functional component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,79 +1,126 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {Grid, Typography, Paper} from '@material-ui/core';
 
 
-
-
-class Profile extends Component {
-
-    constructor(props) {
-        super(props)
+export default function Profile(props) {
+  console.log(props.works)
+  return(
+    <Grid container className={props.classes.container}>
     
-        this.state = {
-          works: this.props.works,
-          number_columns: 3,
-        }
-    }
-
-    componentDidMount(){
-        console.log(this.props)
-    }
-
-    render() { 
-        return(
-            <Grid container className={this.props.classes.container}>
-      
-              <div className="row">
-                <div className="col">
-                  {/* <form>
-                    <div className="form-group">
-                      <label htmlFor="file_upload">Upload Picture</label>
-                      <input type="file" className="form-control-file" id="file_upload" onChange={this.handleFileInputChange} />
+            <div className="row">
+              <div className="col">
+                {/* <form>
+                  <div className="form-group">
+                    <label htmlFor="file_upload">Upload Picture</label>
+                    <input type="file" className="form-control-file" id="file_upload" onChange={this.handleFileInputChange} />
                     </div>
-                  </form> */}
-                </div>
+                </form> */}
               </div>
-      
-              {this.workRows().map((workRow, rowIndex) =>
-                <Grid container item key={`work_row_${rowIndex}`} className="row">
-                  {workRow.map((work, columnIndex) =>
-                    <Grid item xs={4} key={`work_row_${rowIndex}_col_${columnIndex}`} 
-                    component={Paper} elevation={1} className={this.props.classes.workContainer}>
-                        <Typography variant="h6">{work.title}</Typography>
-                        <Grid container item xs={12} className={this.props.classes.imgContainer}>
-                          <img alt={work.title} src={`http://localhost:3000${work.image}`} className={this.props.classes.img} />
-                        </Grid>
-                        <Grid container item xs={12} className={this.props.classes.descContainer}>
-                          <Typography>{work.description}</Typography>
-                        </Grid>
-                    </Grid>
-                  )}
-                </Grid>
-              )}
-            </Grid>
-          )
-    }
-
-    workRows() {
-        let rows = []
-        let row = []
-        this.state.works.reverse().forEach((work) => {
-          row.push(work)
-          if (row.length === this.state.number_columns) {
-            rows.push(row)
-            row = []
-          }
-        })
-        if (row.length > 0) {
-          rows.push(row)
-        }
-        return rows
-      }
+            </div>
     
-
-
-}
-
-
-
-export default Profile;
+            {workRows().map((workRow, rowIndex) =>
+              <Grid container item key={`work_row_${rowIndex}`} className="row">
+                {workRow.map((work, columnIndex) =>
+                  <Grid item xs={4} key={`work_row_${rowIndex}_col_${columnIndex}`} 
+                  component={Paper} elevation={1} className={props.classes.workContainer}>
+                      <Typography variant="h6">{work.title}</Typography>
+                      <Grid container item xs={12} className={props.classes.imgContainer}>
+                        <img alt={work.title} src={`http://localhost:3000${work.image}`} className={props.classes.img} />
+                      </Grid>
+                      <Grid container item xs={12} className={props.classes.descContainer}>
+                        <Typography>{work.description}</Typography>
+                      </Grid>
+                  </Grid>
+                )}
+              </Grid>
+            )}
+          </Grid>
+        )
+        function workRows() {
+          let rows = []
+          let row = []
+          props.works.reverse().forEach((work) => {
+            row.push(work)
+            if (row.length === 3) {
+              rows.push(row)
+              row = []
+            }
+          })
+          if (row.length > 0) {
+            rows.push(row)
+          }
+          return rows
+        }     
+    }
+    
+//--------------------------------------------------------------------------------- 
+//------------------------------------------------------ Class Component Equivelant    
+    
+// class Profile extends React.Component {
+  
+//     constructor(props) {
+//           super(props)
+  
+//           this.state = {
+//               works: this.props.works,
+//               number_columns: 3,
+//             }
+//         }
+    
+//       componentDidMount(){
+//             console.log(this.props)
+//         }
+    
+//       render() { 
+//             return(
+//                   <Grid container className={this.props.classes.container}>
+      
+//                     <div className="row">
+//                       <div className="col">
+//                         {/* <form>
+//                           <div className="form-group">
+//                             <label htmlFor="file_upload">Upload Picture</label>
+//                             <input type="file" className="form-control-file" id="file_upload" onChange={this.handleFileInputChange} />
+//                           </div>
+//                       </form> */}
+//                     </div>
+//                   </div>
+    
+//                   {this.workRows().map((workRow, rowIndex) =>
+//                     <Grid container item key={`work_row_${rowIndex}`} className="row">
+//                       {workRow.map((work, columnIndex) =>
+//                         <Grid item xs={4} key={`work_row_${rowIndex}_col_${columnIndex}`} 
+//                         component={Paper} elevation={1} className={this.props.classes.workContainer}>
+//                             <Typography variant="h6">{work.title}</Typography>
+//                             <Grid container item xs={12} className={this.props.classes.imgContainer}>
+//                               <img alt={work.title} src={`http://localhost:3000${work.image}`} className={this.props.classes.img} />
+//                             </Grid>
+//                             <Grid container item xs={12} className={this.props.classes.descContainer}>
+//                               <Typography>{work.description}</Typography>
+//                         </Grid>
+//                     </Grid>
+//                   )}
+//                 </Grid>
+//               )}
+//             </Grid>
+//           )
+//       }
+  
+//       workRows() {
+//         let rows = []
+//         let row = []
+//         this.state.works.reverse().forEach((work) => {
+//           row.push(work)
+//           if (row.length === this.state.number_columns) {
+//               rows.push(row)
+//               row = []
+//             }
+//         })
+//           if (row.length > 0) {
+//               rows.push(row)
+//             }
+//             return rows
+//           }
+// }
+  
+// export default Profile;
