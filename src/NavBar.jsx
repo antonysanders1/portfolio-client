794dec07d51ad893c8e44e953f0d63ebdc416351
antony_sanders <antony.devon92@gmail.com>
2020-09-24 04:56:48 -0400

    removed router from here

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,17 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom'
 import {Grid, Typography, Divider, Button} from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import {compose} from 'recompose';
 import {connect} from 'react-redux';
-
-import Home from './components/Home';
-import About from './components/About';
-import Login from './components/Login';
-import Account from './components/Account';
-import UploadNewWork from './components/UploadNewWork';
-import FeedContainer from './containers/FeedContainer';
-import ProfileContainer from './containers/ProfileContainer';
 
 import {getUser, logoutUser} from './actions/actions'
 
@@ -48,52 +40,41 @@ class NavBar extends React.PureComponent {
     const {classes} = this.props;
     return (
       <div>
-          <Router>
-                    <Grid container direction="column" className={classes.naviBar}>
-                        <Grid item xs={12} container >
-                        
-                            <Grid item md={1}> </Grid>
+        <Grid container direction="column" className={classes.naviBar}>
+            <Grid item xs={12} container >
+            
+                <Grid item md={1}> </Grid>
 
 
-                            <Grid item xs={6} sm={5} md={3} lg={3}>
-                            <Typography className={classes.navLogo} variant='h5'>Portfoli-Yo!</Typography>
-                            </Grid>
+                <Grid item xs={6} sm={5} md={3} lg={3}>
+                <Typography className={classes.navLogo} variant='h5'>Portfoli-Yo!</Typography>
+                </Grid>
 
-                            <Grid item sm={1} md={1} lg={5}> </Grid>
+                <Grid item sm={1} md={1} lg={5}> </Grid>
 
-                            <Grid item xs={2} sm={2} md={2} lg={1}> 
-                                {
-                                  this.props.user.name ? <Button fullWidth color='primary' component={Link} to="/home">Home</Button> :
-                                  <Button fullWidth color='primary' component={Link} to="/">Home</Button>
-                                } 
-                            </Grid>
+                <Grid item xs={2} sm={2} md={2} lg={1}> 
+                    {
+                      this.props.user.name ? <Button fullWidth color='primary' component={NavLink} to="/home">Home</Button> :
+                      <Button fullWidth color='primary' component={NavLink} to="/">Home</Button>
+                    } 
+                </Grid>
 
-                            <Grid item xs={2} sm={2} md={2} lg={1}>
-                                {
-                                  this.props.user.name ? <Button fullWidth color='primary' component={Link} to={`/${this.props.user.name}`}>Profile</Button> :
-                                  <Button fullWidth color='primary' component={Link} to="/about">About</Button>
-                                }
-                            </Grid>
+                <Grid item xs={2} sm={2} md={2} lg={1}>
+                    {
+                      this.props.user.name ? <Button fullWidth color='primary' component={NavLink} to={`/${this.props.user.name}`}>Profile</Button> :
+                      <Button fullWidth color='primary' component={NavLink} to="/about">About</Button>
+                    }
+                </Grid>
 
-                            <Grid item xs={2} sm={2} md={2} lg={1}>
-                                {
-                                this.props.user.name ? <Button fullWidth color='primary' variant="contained" component={Link} to="/" onClick={this.handleClick}>Logout</Button> : 
-                                <Button fullWidth color='primary' variant="contained" component={Link} to="/login">Login</Button>
-                                } 
-                            </Grid>       
-                        </Grid>
-                        <Divider/>
-                    </Grid>
-                    <Switch>
-                        <Route path="/home" exact component={FeedContainer} />
-                        <Route path={`/${this.props.user.name}`} exact component={ProfileContainer} />
-                        <Route path="/account" exact component={Account} />
-                        <Route path="/upload" exact component={UploadNewWork}/>
-                        <Route path="/login" exact component={Login} />
-                        <Route path="/about" exact component={About} />
-                        <Route path="/" exact component={Home} />
-                    </Switch>
-            </Router>
+                <Grid item xs={2} sm={2} md={2} lg={1}>
+                    {
+                    this.props.user.name ? <Button fullWidth color='primary' variant="contained" component={NavLink} to="/" onClick={this.handleClick}>Logout</Button> : 
+                    <Button fullWidth color='primary' variant="contained" component={NavLink} to="/login">Login</Button>
+                    } 
+                </Grid>       
+            </Grid>
+            <Divider/>
+        </Grid>
       </div>
     );
   }
