9da64e639ad042d76e9eae8cd443432f789eb9e8
antony_sanders <antony.devon92@gmail.com>
2020-09-24 05:06:38 -0400

    added update user reducer

--- src/reducers/userReducer.jsx
+++ src/reducers/userReducer.jsx
@@ -3,6 +3,9 @@ export default (state = {}, action) => {
         case 'CREATE_USER':
             return (action.payload)
 
+        case 'UPDATE_USER':
+            return (action.user)
+
         case 'LOGIN_USER':
             return (action.payload)
 
